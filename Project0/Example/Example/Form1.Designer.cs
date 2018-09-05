02805e8597dd09b710a9216005c952e6c8a5e256
MobsterAres <35581392+MobsterAres@users.noreply.github.com>
2018-09-05 11:26:18 -0500

    Add files via upload

---
+++ Project0/Example/Example/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.win = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // win
+            // 
+            this.win.Location = new System.Drawing.Point(163, 173);
+            this.win.Name = "win";
+            this.win.Size = new System.Drawing.Size(194, 50);
+            this.win.TabIndex = 0;
+            this.win.Text = "You Win!!!";
+            this.win.UseVisualStyleBackColor = true;
+            this.win.Click += new System.EventHandler(this.endButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.win);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button win;
+    }
+}
+
